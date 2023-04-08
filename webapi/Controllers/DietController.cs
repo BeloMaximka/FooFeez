568e24e2d23641e18a186e59df1c05b5675eba21
DDeenis <ddeenis46@gmail.com>
2023-04-08 12:01:57 +0100

    add DietController example

---
+++ webapi/Controllers/DietController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("diet")]
+    public class DietController : ControllerBase
+    {
+        [HttpGet(Name = "menu-calories")]
+        public int GetMenuByCalories()
+        {
+            return 1;
+        }
+    }
+}
