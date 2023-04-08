91db71f9325e961e5a046dce726eceaa3b151e43
DDeenis <ddeenis46@gmail.com>
2023-04-08 13:20:33 +0100

    add di

--- webapi/Controllers/DietController.cs
+++ webapi/Controllers/DietController.cs
@@ -8,6 +8,13 @@ namespace webapi.Controllers
     [Route("api/diet")]
     public class DietController : ControllerBase
     {
+        private readonly IRecipesApi api;
+
+        public DietController(IRecipesApi api)
+        {
+            this.api = api;
+        }
+
         [HttpGet(Name = "GetMenuByCalories")]
         public Menu GetMenuByCalories([FromQuery] int maxCalories)
         {
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,3 +1,6 @@
+using com.spoonacular;
+using Org.OpenAPITools.Client;
+
 namespace webapi
 {
     public class Program
@@ -12,6 +15,10 @@ namespace webapi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // configure spoonacular sdk
+            Configuration.ApiKey.Add("x-api-key", "9181d4bd10bf4430928d63ab271b2774");
+            builder.Services.AddSingleton<IRecipesApi, RecipesApi>();
 
             var app = builder.Build();
 
