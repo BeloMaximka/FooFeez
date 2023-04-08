ad06ca7485905e2fdf27adc2643dec79f13fa3e4
DDeenis <ddeenis46@gmail.com>
2023-04-08 11:50:51 +0100

    add frontend folder

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,23 @@
+// import { Box, Container, Typography } from "@material-ui/core";
+
+import { Box, Container, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI - Next.js example in TypeScript
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
